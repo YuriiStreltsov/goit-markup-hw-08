54200411c5e79befe4d7a8b28ca91fc7473ab6a6
YuriiStreltsov <streltsovyurii86@ukr.net>
2020-07-28 21:24:07 +0300

    Добавляет всплывающее меню для мобилок

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
-  
+      
     menuBtnRef.addEventListener("click", () => {
       const expanded =
         menuBtnRef.getAttribute("aria-expanded") === "true" || false;
@@ -10,5 +10,5 @@
       menuBtnRef.setAttribute("aria-expanded", !expanded);
   
       mobileMenuRef.classList.toggle("is-open");
-    });
+      });
   })();
